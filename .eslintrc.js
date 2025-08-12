15e796a84c8ef1b05f4ecbc818d83f670d304b9b
hunglm <hung97lemanh@gmail.com>
2025-08-12 18:10:23 +0700

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,25 @@ module.exports = {
     root: true,
     env: {
         node: true,
-        browser: true
+        browser: true,
+        es6: true
     },
+    extends: ["eslint:recommended", "next/core-web-vitals"],
     rules: {
-        // This disables all rules
+        // Keep your disabled rules
         "no-unused-vars": "off",
-        "no-console": "off"
+        "no-console": "off",
+
+        // NextJS specific rules that might need disabling
+        "@next/next/no-img-element": "off",
+        "@next/next/no-html-link-for-pages": "off"
     },
-    // Set parserOptions to empty to prevent parsing
-    parserOptions: {},
-    // Disable all plugins and extends
-    plugins: [],
-    extends: []
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: ["react"]
 };
